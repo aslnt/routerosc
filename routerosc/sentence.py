3e5b138cbab91f875b6ae4f6ca8035f181e5bce4
aslnt <237437868+aslnt@users.noreply.github.com>
2026-03-15 23:53:46 +0300

    Refine `read_length()`: use bitwise operations for consistency

--- routerosc/sentence.py
+++ routerosc/sentence.py
@@ -44,11 +44,11 @@ async def read_length(reader):
     if byte < b'\x80':
         return int.from_bytes(byte)
     if byte < b'\xc0':
-        return int.from_bytes(byte + await reader.readexactly(1)) - 0x8000
+        return int.from_bytes(byte + await reader.readexactly(1)) ^ 0x8000
     if byte < b'\xe0':
-        return int.from_bytes(byte + await reader.readexactly(2)) - 0xC00000
+        return int.from_bytes(byte + await reader.readexactly(2)) ^ 0xC00000
     if byte < b'\xf0':
-        return int.from_bytes(byte + await reader.readexactly(3)) - 0xE0000000
+        return int.from_bytes(byte + await reader.readexactly(3)) ^ 0xE0000000
     if byte == b'\xf0':
         return int.from_bytes(await reader.readexactly(4))
 
