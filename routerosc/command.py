56542a38a624bc5a78445ab124f46d03bf493ec2
aslnt <237437868+aslnt@users.noreply.github.com>
2026-03-17 12:51:24 +0300

    Add `<=` query operator

--- routerosc/command.py
+++ routerosc/command.py
@@ -62,5 +62,8 @@ def dump_query(query):
         case ['>=', an, av]:
             yield from dump_query(('|', ('>', an, av), ('=', an, av)))
 
+        case ['<=', an, av]:
+            yield from dump_query(('|', ('<', an, av), ('=', an, av)))
+
         case _:
             raise ValueError(f"Bad query: {query!r}")
